3401a872231dfa5971045dfd47342fb6b5f8c4e1
Bangarraju <bangarraju@absolinsoft.com>
2024-10-09 18:23:42 +0530

    swagger

--- config/swagger.ts
+++ config/swagger.ts
@@ -1,41 +1,41 @@
 import path from "node:path";
 import url from "node:url";
-import env from '#start/env'
+import env from '#start/env';
 
 export default {
   path: path.dirname(url.fileURLToPath(import.meta.url)) + "/../",
   tagIndex: 2,
   info: {
-    title: "Node Base Project",
+    title: "User Managament",
     version: "1.0.0",
-    description: "This is a node base project",
+    description: "This is a User Managament project",
   },
   components: {
-    securitySchemas: {
+    securitySchemes: {
       bearerAuth: {
         type: 'http',
-        schema: 'bearer',
+        scheme: 'bearer',
         bearerFormat: 'JWT'
       }
     }
   },
   snakeCase: true,
   debug: env.get('DEBUG'), // set to true, to get some useful debug output
-  ignore: ["/swagger", "/docs", "/group/permissions", "/users/su"],
+  ignore: ["/swagger", "/scalar", "/rapidoc", "/docs"],
   preferredPutPatch: "PUT", // if PUT/PATCH are provided for the same route, prefer PUT
   common: {
     parameters: {}, // OpenAPI conform parameters that are commonly used
     headers: {}, // OpenAPI conform headers that are commonly used
   },
   securitySchemes: {
-    ApiKeyAuth: {
-      type: "apiKey",
-      in: "header",
-      name: "X-API-Key"
+    BearerAuth: {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
     }
-  }, // optional
-  authMiddlewares: ["auth", "auth:api"], // optional
+  },
   defaultSecurityScheme: "BearerAuth", // optional
   persistAuthorization: true, // persist authorization between reloads on the swagger page
   showFullPath: true, // the path displayed after endpoint summary
 };
+
