70230c1c3bab3c507aa5bc22fc8e096923ae9d27
Bangarraju <bangarraju@absolinsoft.com>
2024-10-10 14:40:29 +0530

    adminjs login panel

---
+++ app/admin/auth.ts
@@ -0,0 +1,26 @@
+import { DefaultAuthProvider } from 'adminjs'
+
+import componentLoader from './component_loader.js'
+import { DEFAULT_ADMIN } from "./constants.js"
+
+/**
+ * Your "authenticate" function. Depending on the auth provider used, the payload may be different.
+ *
+ * The default authentication provider uses email and password to authenticate. You can modify this
+ * function to use email & password to verify if the User exists and if their passwords match.
+ *
+ * The default implementation below will let any in, so make sure to update it.
+ */
+
+const provider = new DefaultAuthProvider({
+  componentLoader,
+  authenticate: async ({ email, password }) => {
+    if (email === DEFAULT_ADMIN.email) {
+      return { email };
+    }
+
+    return null;
+  },
+});
+
+export default provider;
--- config/database.ts
+++ config/database.ts
@@ -1,22 +1,48 @@
 import "reflect-metadata"
 import env from '#start/env'
-
+import { defineConfig } from '@adonisjs/lucid'
 import { DataSource } from "typeorm"
 import { AuthGroup, AuthGroupPermissions, AuthPermission, AuthUser, UserGroup, AuthToken } from "#models/index"
 
+const dbCredentials = {
+  host: env.get('DB_HOST'),
+  port: Number(env.get('DB_PORT')),
+  username: env.get('DB_USER') as string,
+  password: env.get('DB_PASS') as string,
+  database: env.get('DB_NAME') as string,
+}
+
+const db: any = {
+  connection: 'postgres',
+  connections: {
+    postgres: {
+      client: 'pg',
+      connection: dbCredentials,
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+  },
+}
+
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: env.get('DB_HOST'),
-    port: env.get('DB_PORT'),
-    username: env.get('DB_USER'),
-    password: env.get('DB_PASS'),
-    database: env.get('DB_NAME'),
-    logging: env.get('DEBUG'),
-    entities: [AuthGroup, AuthPermission, AuthGroupPermissions, AuthUser, UserGroup, AuthToken],
-    subscribers: [],
-    synchronize: env.get('DEBUG'),
+  type: "postgres",
+  ...dbCredentials,
+  logging: env.get('DEBUG'),
+  entities: [AuthGroup, AuthPermission, AuthGroupPermissions, AuthUser, UserGroup, AuthToken],
+  subscribers: [],
+  synchronize: env.get('DEBUG'),
 })
 
 AppDataSource.initialize().then(async () => {
-    console.log("db connected !...")
-}).catch(error => { console.log(error); process.exit(1) })
+  console.log("db connected !...")
+}).catch(error => {
+  console.log(error);
+  process.exit(1);
+})
+
+const dbConfig = defineConfig(db)
+export default dbConfig
+
+
